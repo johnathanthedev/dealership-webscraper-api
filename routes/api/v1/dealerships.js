de0d019f4a55aaf976f4cc05c373d5e9e0c8fd1d
johnathanthedev <johnathanguzman17@gmail.com>
2021-06-27 19:17:25 -0500

    Added zip check and some custom validations.

--- routes/api/v1/dealerships.js
+++ routes/api/v1/dealerships.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 const google_scrape = require("../../../services/webscrape/google_scrape")
 
-router.get("/test", async (req, res) => {
-        const google_results = await google_scrape(req.params.user_zip)
+router.get("/", async (req, res) => {
+        const user_zip = req.headers.user_zip
+        const google_results = await google_scrape(user_zip)
         res.json(google_results)
 })
 
